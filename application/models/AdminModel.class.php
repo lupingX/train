c2495897b2b3a070a97d61617ee71681af7c7ef2
lupingX <xiangluping93@hotmail.com>
2016-09-12 10:59:20 +0100

    change some page: not function changed

--- application/models/AdminModel.class.php
+++ application/models/AdminModel.class.php
@@ -6,7 +6,7 @@ class AdminModel extends Model{
 		return $this->db->getAll($sql);
 	}
 	public function checkAdmins($username, $password){
-		$password=md5( $password);
+		// $password=md5( $password);
 		// echo $password;
 		$sql = "SELECT * FROM {$this -> table } WHERE  admin_name ='$username' AND password= '$password' LIMIT 1";
 		// echo $sql;
